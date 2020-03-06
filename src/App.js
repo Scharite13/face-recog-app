7c8812ea525fb12683f0b48820d153fcc3429b07
Derek Scharite <dms121691@gmail.com>
2020-03-06 14:26:19 -0500

    fixed image url

--- src/App.js
+++ src/App.js
@@ -101,7 +101,7 @@ class App extends Component {
     .then(response => response.json())
         .then(response => {
           if (response) {
-            fetch('https://mighty-thicket-84914.herokuapp.com:3001/image', {
+            fetch('https://mighty-thicket-84914.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
